50c0a0764f14c3b6045a2a311a3bc97526a0508d
kqlqk <kononchuk99999@gmail.com>
2022-10-15 10:46:02 +0200

    init

---
+++ src/main/java/me/kqlqk/behealthy/cloudconfig/CloudConfigApplication.java
@@ -0,0 +1,21 @@
+package me.kqlqk.behealthy.cloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CloudConfigApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CloudConfigApplication.class);
+    }
+}
