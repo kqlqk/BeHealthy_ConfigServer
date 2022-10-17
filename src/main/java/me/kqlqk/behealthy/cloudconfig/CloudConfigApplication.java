fb739dad2bf46792d23e0ff4ef4312a98c065e96
kqlqk <kononchuk99999@gmail.com>
2022-10-17 19:26:21 +0200

    update application.properties
    * was updated application.properties

--- src/main/java/me/kqlqk/behealthy/cloudconfig/CloudConfigApplication.java
+++ src/main/java/me/kqlqk/behealthy/cloudconfig/CloudConfigApplication.java
@@ -6,17 +6,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-import java.util.Map;
-
 @SpringBootApplication
 @EnableConfigServer
 public class CloudConfigApplication extends SpringBootServletInitializer {
-
     public static void main(String[] args) {
-        for (Map.Entry<String, String> entry : System.getenv().entrySet()) {
-            System.out.println(entry.getKey() + ":" + entry.getValue());
-        }
-
         SpringApplication.run(CloudConfigApplication.class, args);
     }
 
